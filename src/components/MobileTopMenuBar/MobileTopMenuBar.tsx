38775493b23cdd319b3fa1c23bbfd5de48adb6b1
Zoran Curcin <zorancurchin@gmail.com>
2022-05-03 12:15:44 +0200

    fixed layout for mobile top menu when minimize button is present and room name is adhok

--- src/components/MobileTopMenuBar/MobileTopMenuBar.tsx
+++ src/components/MobileTopMenuBar/MobileTopMenuBar.tsx
@@ -9,9 +9,9 @@ import Menu from '../MenuBar/Menu/Menu';
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
     background: 'white',
-    paddingLeft: '1em',
+    padding: '1em',
     display: 'none',
-    height: `${theme.mobileTopBarHeight}px`,
+    // height: `${theme.mobileTopBarHeight}px`,
     [theme.breakpoints.down('sm')]: {
       display: 'flex',
     },
@@ -43,7 +43,8 @@ export default function MobileTopMenuBar() {
     <Grid container alignItems="center" justifyContent="space-between" className={classes.container}>
       <Typography variant="subtitle1">{roomInfo?.title}</Typography>
       <div>
-        {isInIframe && <ToggleSizeButton className={classes.endCallButton} />}
+        {/* {isInIframe && <ToggleSizeButton className={classes.endCallButton} />} */}
+        <ToggleSizeButton className={classes.endCallButton} />
         <EndCallButton className={classes.endCallButton} />
         <Menu buttonClassName={classes.settingsButton} />
       </div>
